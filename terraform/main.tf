91c7f08a90bae54da8819e1192ac5543fdd84b44
stoppie <kardach.michal@gmail.com>
2023-08-26 15:30:02 +0200

    Added MySQL instance to Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,20 @@ resource "google_compute_network" "wiki_assistant_vpc" {
   routing_mode            = "REGIONAL"
 }
 
+resource "google_compute_global_address" "private_ip_address" {
+  name          = "wiki-assistant-private-ip-range"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.wiki_assistant_vpc.self_link
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.wiki_assistant_vpc.self_link
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
 resource "google_compute_firewall" "wiki_assistant_allow_icmp" {
   name    = "wiki-assistant-vpc-allow-icmp"
   network = google_compute_network.wiki_assistant_vpc.name
@@ -18,9 +32,9 @@ resource "google_compute_firewall" "wiki_assistant_allow_icmp" {
     protocol = "icmp"
   }
 
-  description = "Allows ICMP connections from any source to any instance on the network."
-  direction   = "INGRESS"
-  priority    = 65534
+  description   = "Allows ICMP connections from any source to any instance on the network."
+  direction     = "INGRESS"
+  priority      = 65534
   source_ranges = ["0.0.0.0/0"]
 }
 
@@ -33,8 +47,64 @@ resource "google_compute_firewall" "wiki_assistant_allow_ssh" {
     ports    = ["22"]
   }
 
-  description = "Allows TCP connections from any source to any instance on the network using port 22."
-  direction   = "INGRESS"
-  priority    = 65534
+  description   = "Allows TCP connections from any source to any instance on the network using port 22."
+  direction     = "INGRESS"
+  priority      = 65534
   source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_sql_database_instance" "wiki_assistant_db" {
+  name             = "wiki-assistant-db"
+  database_version = "MYSQL_8_0_31"
+  region           = "us-central1"
+  root_password    = var.mysql_root_password
+
+  settings {
+    tier                        = "db-custom-1-3840"
+    deletion_protection_enabled = true
+    disk_autoresize             = false
+    disk_size                   = 20
+    disk_type                   = "PD_SSD"
+    activation_policy           = "ALWAYS"
+    availability_type           = "ZONAL"
+    pricing_plan                = "PER_USE"
+
+    backup_configuration {
+      location           = "us"
+      binary_log_enabled = true
+      enabled            = true
+      start_time         = "04:00"
+
+      backup_retention_settings {
+        retained_backups = 7
+      }
+
+      transaction_log_retention_days = 7
+    }
+
+    location_preference {
+      zone = "us-central1-b"
+    }
+
+    ip_configuration {
+      private_network                               = "projects/wikipedia-assistant-397017/global/networks/wiki-assistant-vpc"
+      ipv4_enabled                                  = false
+      allocated_ip_range                            = "wiki-assistant-private-ip-range"
+      enable_private_path_for_google_cloud_services = true
+    }
+
+    insights_config {
+      query_insights_enabled = false
+    }
+
+    maintenance_window {
+      day          = 5
+      hour         = 22
+      update_track = "stable"
+    }
+
+    edition = "ENTERPRISE"
+  }
+
+  timeouts {}
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "mysql_root_password" {
+  description = "The MySQL database password"
+  type        = string
+  sensitive   = true
+}
