5845b205ff5b359401927328c22410d43ff62846
stoppie <kardach.michal@gmail.com>
2023-08-29 13:51:16 +0200

    Added transactions to the SQL script

--- database/import_wiki.sh
+++ database/import_wiki.sh
@@ -17,12 +17,27 @@ urls=(
     "https://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-categorylinks.sql.gz"
 )
 
+MAX_RETRIES=3
+
 # Loop over each URL and import them
 for url in "${urls[@]}"; do
     filename="$(basename "$url")"
     sql_filename="${filename%.gz}"
     
-    wget "$url"
+    # Retry mechanism for downloads
+    retry_count=0
+    until [ $retry_count -ge $MAX_RETRIES ]
+    do
+        wget "$url" && break
+        retry_count=$[$retry_count+1]
+        sleep 5
+    done
+
+    if [ ! -f "$filename" ]; then
+        echo "Failed to download $url after $MAX_RETRIES attempts."
+        exit 1
+    fi
+
     gunzip "$filename"
     mysql -h $DATABASE_IP_ADDRESS -u $DATABASE_USER $DATABASE_NAME < "$sql_filename"
 
--- database/sql/postprocessing.sql
+++ database/sql/postprocessing.sql
@@ -29,99 +29,140 @@ CREATE TABLE IF NOT EXISTS `pagelinks` (
 
 -- DATA LOADS
 
-INSERT INTO `page` (`page_id`, `page_namespace`, `page_title`, `page_touched`)
-SELECT
-    page_id,
-    page_namespace,
-    page_title,
-    STR_TO_DATE(CONVERT(page_touched USING utf8), '%Y%m%d%H%i%s')
-FROM `wiki_staging`.`page`
-ON DUPLICATE KEY UPDATE 
-  page_namespace = VALUES(page_namespace),
-  page_title = VALUES(page_title),
-  page_touched = VALUES(page_touched);
+DELIMITER //
 
-INSERT IGNORE INTO `categorylinks` (`cl_from`, `cl_to`) -- PAGE_ID=1039753 MISSING IN THE PAGE TABLE
-SELECT
-    cl_from,
-    cl_to
-FROM `wiki_staging`.`categorylinks`;
+CREATE PROCEDURE IF NOT EXISTS LoadData()
+BEGIN
+    -- Declare an exit handler for SQLEXCEPTION
+    -- This will be invoked if any SQL error occurs inside the block
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        -- If any SQL error occurs, rollback the transaction
+        ROLLBACK;
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'An error occurred, transaction rolled back.';
+    END;
 
-INSERT IGNORE INTO `pagelinks` (`pl_from`, `pl_namespace`, `pl_title`) -- PAGE_ID=1039753 MISSING IN THE PAGE TABLE
-SELECT
-    pl_from,
-    pl_namespace,
-    pl_title
-FROM `wiki_staging`.`pagelinks`;
+    -- Start transaction
+    START TRANSACTION;
+
+    INSERT INTO `page` (`page_id`, `page_namespace`, `page_title`, `page_touched`)
+    SELECT
+        page_id,
+        page_namespace,
+        page_title,
+        STR_TO_DATE(CONVERT(page_touched USING utf8), '%Y%m%d%H%i%s')
+    FROM `wiki_staging`.`page`
+    ON DUPLICATE KEY UPDATE 
+        page_namespace = VALUES(page_namespace),
+        page_title = VALUES(page_title),
+        page_touched = VALUES(page_touched);
+
+    INSERT IGNORE INTO `categorylinks` (`cl_from`, `cl_to`) -- PAGE_ID=1039753 MISSING IN THE PAGE TABLE
+    SELECT
+        cl_from,
+        cl_to
+    FROM `wiki_staging`.`categorylinks`;
+
+    INSERT IGNORE INTO `pagelinks` (`pl_from`, `pl_namespace`, `pl_title`) -- PAGE_ID=1039753 MISSING IN THE PAGE TABLE
+    SELECT
+        pl_from,
+        pl_namespace,
+        pl_title
+    FROM `wiki_staging`.`pagelinks`;
+    COMMIT;
+END;
+//
 
 -- ADDITIONAL TABLES FOR THE API ENDPOINT
 
-DROP TABLE IF EXISTS `pageoutdatedness`;
+CREATE PROCEDURE IF NOT EXISTS UpdateOutdatednessRankings()
+BEGIN
+    -- Declare an exit handler for SQLEXCEPTION
+    -- This will be invoked if any SQL error occurs inside the block
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        -- If any SQL error occurs, rollback the transaction
+        ROLLBACK;
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'An error occurred, transaction rolled back.';
+    END;
 
-CREATE TABLE `pageoutdatedness` (
-  `page_id` int NOT NULL DEFAULT '0',
-  `page_behind` int NOT NULL,
-  PRIMARY KEY (`page_id`)
-);
+    -- Start transaction
+    START TRANSACTION;
 
-INSERT INTO `pageoutdatedness` (`page_id`, `page_behind`)
-SELECT
-    p_source.page_id,
-    MAX(TIMESTAMPDIFF(SECOND, p_target.page_touched, p_source.page_touched))
-FROM `page` p_source
-JOIN `pagelinks` pl ON
-    p_source.page_id = pl.pl_from
-JOIN `page` p_target ON
-    pl.pl_namespace = p_target.page_namespace
-    AND pl.pl_title = p_target.page_title
-GROUP BY
-    p_source.page_id
-HAVING
-    MAX(TIMESTAMPDIFF(SECOND, p_target.page_touched, p_source.page_touched)) > 0;
+    DROP TABLE IF EXISTS `pageoutdatedness`;
+    DROP TABLE IF EXISTS `categoryoutdated`;
 
-DROP TABLE IF EXISTS `categoryoutdated`;
+    CREATE TABLE `pageoutdatedness` (
+        `page_id` int NOT NULL DEFAULT '0',
+        `page_behind` int NOT NULL,
+        PRIMARY KEY (`page_id`)
+    );
 
-CREATE TABLE `categoryoutdated` (
-  `category` varbinary(255) NOT NULL DEFAULT '',
-  `page_id` int NOT NULL,
-  `page_namespace` int NOT NULL DEFAULT '0',
-  `page_title` varbinary(255) NOT NULL DEFAULT '',
-  `page_outdatedness` int NOT NULL,
-  PRIMARY KEY (`category`)
-);
+    CREATE TABLE `categoryoutdated` (
+        `category` varbinary(255) NOT NULL DEFAULT '',
+        `page_id` int NOT NULL,
+        `page_namespace` int NOT NULL DEFAULT '0',
+        `page_title` varbinary(255) NOT NULL DEFAULT '',
+        `page_outdatedness` int NOT NULL,
+        PRIMARY KEY (`category`)
+    );
 
-INSERT INTO `categoryoutdated` (`category`, `page_id`, `page_namespace`, `page_title`, `page_outdatedness`)
-WITH categorycounts AS (
+    INSERT INTO `pageoutdatedness` (`page_id`, `page_behind`)
     SELECT
-        cl.cl_to category,
-        COUNT(*) cnt
-    FROM `categorylinks` cl
-    GROUP BY cl.cl_to
-), rankedpages AS (
+        p_source.page_id,
+        MAX(TIMESTAMPDIFF(SECOND, p_target.page_touched, p_source.page_touched))
+    FROM `page` p_source
+    JOIN `pagelinks` pl ON
+        p_source.page_id = pl.pl_from
+    JOIN `page` p_target ON
+        pl.pl_namespace = p_target.page_namespace
+        AND pl.pl_title = p_target.page_title
+    GROUP BY
+        p_source.page_id
+    HAVING
+        MAX(TIMESTAMPDIFF(SECOND, p_target.page_touched, p_source.page_touched)) > 0;
+
+    INSERT INTO `categoryoutdated` (`category`, `page_id`, `page_namespace`, `page_title`, `page_outdatedness`)
+    WITH categorycounts AS (
+        SELECT
+            cl.cl_to category,
+            COUNT(*) cnt
+        FROM `categorylinks` cl
+        GROUP BY cl.cl_to
+    ), rankedpages AS (
+        SELECT
+            cl.cl_to category,
+            cl.cl_from page_id,
+            po.page_behind page_outdatedness,
+            p.page_namespace,
+            p.page_title,
+            DENSE_RANK() OVER (PARTITION BY cl.cl_to ORDER BY po.page_behind DESC) rnk
+        FROM `categorylinks` cl
+        JOIN `pageoutdatedness` po ON
+            cl.cl_from = po.page_id
+        JOIN `page` p ON
+            cl.cl_from = p.page_id
+    )
     SELECT
-        cl.cl_to category,
-        cl.cl_from page_id,
-        po.page_behind page_outdatedness,
-        p.page_namespace,
-        p.page_title,
-        DENSE_RANK() OVER (PARTITION BY cl.cl_to ORDER BY po.page_behind DESC) rnk
-    FROM `categorylinks` cl
-    JOIN `pageoutdatedness` po ON
-        cl.cl_from = po.page_id
-    JOIN `page` p ON
-        cl.cl_from = p.page_id
-)
-SELECT
-    rp.category,
-    rp.page_id,
-    rp.page_namespace,
-    rp.page_title,
-    rp.page_outdatedness
-FROM rankedpages rp
-JOIN categorycounts cc ON
-    rp.category = cc.category
-WHERE
-    rp.rnk = 1
-ORDER BY
-    cc.cnt DESC
-LIMIT 10;
+        rp.category,
+        rp.page_id,
+        rp.page_namespace,
+        rp.page_title,
+        rp.page_outdatedness
+    FROM rankedpages rp
+    JOIN categorycounts cc ON
+        rp.category = cc.category
+    WHERE
+        rp.rnk = 1
+    ORDER BY
+        cc.cnt DESC
+    LIMIT 10;
+    COMMIT;
+END;
+//
+
+DELIMITER ;
+
+-- Call the procedure to execute the operations
+CALL LoadData();
+CALL UpdateOutdatednessRankings();
